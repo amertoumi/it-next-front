a2876b18ef638ccef22ef33454720af458e60253
amertoumi <amer.toumi@agence-inspire.com>
2021-04-29 13:46:53 +0100

    fix api url entry point

--- API.js
+++ API.js
@@ -1,5 +1,5 @@
-export const API_HOST = 'http://127.0.0.1:8000';
-//export const API_HOST = 'http://api.it.prod-projet.com';
+//export const API_HOST = 'http://127.0.0.1:8000';
+export const API_HOST = 'http://api.it.prod-projet.com';
 export const API_SKILLS_PATH = '/api/skills';
 export const API_AUTH_PATH = '/api/login';
 export const API_USERS_PATH = '/api/users';
